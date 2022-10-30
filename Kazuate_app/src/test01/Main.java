7d15ab92d5f5ba5d632f0a19f674ed1ac210063e
hiroshi-git147 <116545212+hiroshi-git147@users.noreply.github.com>
2022-10-31 08:31:29 +0900

    Update Main.java

--- Kazuate_app/src/test01/Main.java
+++ Kazuate_app/src/test01/Main.java
@@ -1,40 +1,18 @@
-package kazuate;
+package test00;
 
-public class Test {
+import java.util.Scanner;
+
+public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		int[] comArray = comGuess(4);
-		for(int i = 0; i < comArray.length; i++) {
-			System.out.println(i + ":" + comArray[i]);
-		}
+		int i = guessNumber(0);
+		System.out.println(i);
 		
 	}
 	
-	// コンピューターが無作為に作成した４桁の数字メソッド
-    public static int[] comGuess(int size) {
-    	int count = 4;
-    	// 10個の要素を持つ配列を作成
-    	int[] comNum = new int[size];
-
-    	// 4回繰り返す
-        for(int i = 0; i < count; i++) {
-
-	        Loop: while(true) {
-	        	// trueの場合0～9の数字を生成
-	        	comNum[i] =new java.util.Random().nextInt(9);
-
-	        	for(int j = 0; j < i; j++) {
-	        		// その前までの数値が同じ数値の場合数値代入からやり直し
-	        		if(comNum[j] == comNum[i]) continue Loop;
-	        	}
-	        	break;
-	        }
-
-        }
-
-        return comNum;
-
-    }
-
+	public static int guessNumber(int number) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		return n;
+	}
 }
