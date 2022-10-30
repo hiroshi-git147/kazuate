8d2cbe15a5ecddc2a8af8e4b76f58905c794ab80
hiroshi-git147 <116545212+hiroshi-git147@users.noreply.github.com>
2022-10-31 08:30:32 +0900

    Update Main.java

--- Kazuate_app/src/test01/Main.java
+++ Kazuate_app/src/test01/Main.java
@@ -1,32 +1,40 @@
-package test01;
+package kazuate;
 
-import java.util.Scanner;
+public class Test {
 
-public class Main{
-    public static void main(String[] args){
-    	int[] array = getUserNumber(4);
-//    	for(int i = 0; i < array.length; i++) {
-//    		System.out.print(array[i]);
-//    	}
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int[] comArray = comGuess(4);
+		for(int i = 0; i < comArray.length; i++) {
+			System.out.println(i + ":" + comArray[i]);
+		}
+		
+	}
+	
+	// コンピューターが無作為に作成した４桁の数字メソッド
+    public static int[] comGuess(int size) {
+    	int count = 4;
+    	// 10個の要素を持つ配列を作成
+    	int[] comNum = new int[size];
 
-     }
+    	// 4回繰り返す
+        for(int i = 0; i < count; i++) {
 
-    public static int[] getUserNumber(int size) {
-    	Scanner sc = new Scanner(System.in);
-    	int[] userNum = new int[size];
-    	System.out.println("4桁の数字を入力してください");
-    	int user;
-    	while(true) {
-    		try {
-	    		user = sc.nextInt();
-//	    		userNum.add(user);
-	    		break;
-	    	} catch(Exception e) {
-				System.out.println("数字を入力してください");
-				sc.next();
-	    	}
-    	}
+	        Loop: while(true) {
+	        	// trueの場合0～9の数字を生成
+	        	comNum[i] =new java.util.Random().nextInt(9);
 
-    	return userNum;
+	        	for(int j = 0; j < i; j++) {
+	        		// その前までの数値が同じ数値の場合数値代入からやり直し
+	        		if(comNum[j] == comNum[i]) continue Loop;
+	        	}
+	        	break;
+	        }
+
+        }
+
+        return comNum;
+
     }
+
 }
