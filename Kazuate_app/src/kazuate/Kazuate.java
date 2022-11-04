c640a320cd627e14b57a34c40d236ff7e47bc6af
hiroshi-git147 <116545212+hiroshi-git147@users.noreply.github.com>
2022-11-04 17:55:51 +0900

    Update Kazuate.java

--- Kazuate_app/src/kazuate/Kazuate.java
+++ Kazuate_app/src/kazuate/Kazuate.java
@@ -20,27 +20,25 @@ public class Kazuate{
     	System.out.println("4桁の数字を予想してください");
     	System.out.print("[入力]＞");
 
-		int[] userAnswer = userGuess();
+	int[] userAnswer = userGuess();
 
-		// 判定
-		for(int i = 0; i < userAnswer.length; i++) {
-			while(comAnswer[i] != userAnswer[i]) {
-				// カウントを1増やす
+	// 判定
+	for(int i = 0; i < userAnswer.length; i++) {
+		while(comAnswer[i] != userAnswer[i]) {
+			// カウントを1増やす
 	    		count++;
 	    		// ヒントをもらう
 	    		getHint(comAnswer, userAnswer);
 	    		// 再度入力
 	    		userAnswer = userGuess();
-			}
+		}
     	}
-		System.out.println("「正解です！」");
-		System.out.println("あなたは" + count + "回で正解しました。」");
-		System.out.print("パソコンの答えは：");
+	System.out.println("「正解です！」");
+	System.out.println("あなたは" + count + "回で正解しました。」");
+	System.out.print("パソコンの答えは：");
     	for (int com : comAnswer){
     		System.out.print(com);
-
     	}
-
      }
 
     // コンピューターが無作為に作成した４桁の数字メソッド
@@ -64,10 +62,10 @@ public class Kazuate{
 
     // コンソールから入力するプログラム
     public static int guessNumber(int number) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		return n;
-	}
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
+	return n;
+　　}
 
     // コンソールから入力した4桁の数字を配列に変換
     public static int[] userGuess() {
@@ -75,29 +73,28 @@ public class Kazuate{
 
     	int user = guessNumber(ANSWER_SIZE);
     	// 入力の判定
-		if (user < 1000 || user > 9999) {
-			System.out.println("エラー：4桁の数字を入力してください");
-			guessNumber(ANSWER_SIZE);
-		}
+	if (user < 1000 || user > 9999) {
+		System.out.println("エラー：4桁の数字を入力してください");
+		guessNumber(ANSWER_SIZE);
+	}
 
-		for(int i = 0; i < array.length; i++) {
-			// 入力した数字の1〜4桁の数字を配列0〜3に代入するプログラム
-			switch (i) {
-				case (0):
-					array[i] = user / 1000;
-					break;
-				case 1:
-					array[i] = user / 100 - (user / 1000 * 10);
-					break;
-				case 2:
-					array[i] = user / 10 - (user / 100 * 10);
-					break;
-				case 3:
-					array[i] = user % 10;
-					break;
-			}
+	for(int i = 0; i < array.length; i++) {
+		// 入力した数字の1〜4桁の数字を配列0〜3に代入するプログラム
+		switch (i) {
+			case (0):
+				array[i] = user / 1000;
+				break;
+			case 1:
+				array[i] = user / 100 - (user / 1000 * 10);
+				break;
+			case 2:
+				array[i] = user / 10 - (user / 100 * 10);
+				break;
+			case 3:
+				array[i] = user % 10;
+				break;
 		}
-
+	}
     	return array;
     }
 
@@ -105,22 +102,21 @@ public class Kazuate{
     public static void getHint(int[] comAnswer, int[] userAnswer) {
     	String total;
     	for(int i = 0; i < userAnswer.length; i++) {
-			if(comAnswer[i] == userAnswer[i]) {
-				total = "○";
-			} else {
-				total = "X";
-			}
-			for(int j = 0; j < comAnswer.length; j++) {
-				if(comAnswer[j] == userAnswer[i]) {
-					total = total.replace("X", "△");
-				}
-			}
-			System.out.print(total);
+		if(comAnswer[i] == userAnswer[i]) {
+			total = "○";
+		} else {
+			total = "X";
 		}
-		// 数を再入力
-		System.out.println("");
-		System.out.print("[入力]＞");
+		for(int j = 0; j < comAnswer.length; j++) {
+			if(comAnswer[j] == userAnswer[i]) {
+				total = total.replace("X", "△");
+			}
+		}
+		System.out.print(total);
+	}
+	// 数を再入力
+	System.out.println("");
+	System.out.print("[入力]＞");
     }
-
 
 }
